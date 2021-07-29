3c1b3dde539c454c5a5fa8bebccc0957d8c45527
MYSJ <473764445@qq.com>
2021-07-29 10:45:56 +0800

    1

--- src/com/mysj/sort/Sort.java
+++ src/com/mysj/sort/Sort.java
@@ -8,7 +8,8 @@ public class Sort {
     public static void main(String[] args) {
         int[] input = {12,2,18,9,5,7};
         //bubbleSort(input);
-        selectSort(input);
+        //selectSort(input);
+        insertSort(input);
         for (int i : input) {
             System.out.println(i);
         }
@@ -23,7 +24,6 @@ public class Sort {
      * @param input
      */
     public static void bubbleSort(int[] input){
-        System.out.println("hello");
         for (int i = 0; i < input.length; i++) {
             //优化算法，当第一次循环的时候没有发生交换，就说明有序，不执行后续
             boolean isSort = true;
@@ -63,6 +63,29 @@ public class Sort {
             temp = input[i];
             input[i] = input[min];
             input[min] = temp;
+        }
+    }
+
+    /**
+     * 实现冒泡排序
+     * 时间复杂度    时间复杂度（最坏）   时间复杂度（最好）   空间复杂度   稳定性
+     * O(N^2)       O(N^2)              O(N)              o(1)        稳定
+     *  升序
+     * @param input
+     */
+    public static void insertSort(int[] input){
+        for (int i = 1; i < input.length; i++) {
+            int temp = input[i];
+            int j = 0;
+            for (  j = i-1; j >= 0; j--) {
+                if (temp<input[j]){
+                    //往后移动
+                    input[j+1] = input[j];
+                }else {
+                    break;
+                }
+            }
+            input[j+1] = temp;
         }
     }
 
